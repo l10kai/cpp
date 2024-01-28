81f2b421b572dd9edeadc47fc9a3d7df501115c6
l10kai <l10kai2005@gmail.com>
2024-01-28 16:34:28 +0800

    add

--- codeforces/231109/c2.cpp
+++ codeforces/231109/c2.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    int l = 0, r = 0, ans = 0;
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='(') l++;
-        if(s[i]==')'&& l-r>0) r++;
-    }
+  string s;
+  cin >> s;
+  int l = 0, r = 0, ans = 0;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '(')
+      l++;
+    if (s[i] == ')' && l - r > 0)
+      r++;
+  }
 
-    cout<<r*2;
-    return 0;
+  cout << r * 2;
+  return 0;
 }
-
