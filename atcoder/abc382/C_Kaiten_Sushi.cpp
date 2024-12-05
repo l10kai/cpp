32de801b8989c8c3e66bf90b79a053233fab8987
l10kai <l10kai2005@gmail.com>
2024-12-05 22:22:02 +0800

    更新abc382 C_Kaiten_Sushi.cpp

--- atcoder/abc382/C_Kaiten_Sushi.cpp
+++ atcoder/abc382/C_Kaiten_Sushi.cpp
@@ -8,16 +8,31 @@ using u64 = uint64_t;
 const char &ln = '\n';
 
 typedef pair<i64, i64> pll;
-
+i64 N = 2e5 + 5;
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n, m;
     cin >> n >> m;
+    vector<int> ans(N, -1);
+    i64 idx = N - 5;
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
 
-    vector<int> a(n);
-    
+        while (t <= idx) {
+            ans[idx] = i + 1;
+            idx--;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        int t;
+        cin >> t;
+
+        cout << ans[t] << ln;
+    }
 
     return 0;
 }
