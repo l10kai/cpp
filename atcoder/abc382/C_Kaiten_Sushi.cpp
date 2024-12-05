90453ed5209f839af300fb85701305574a317b39
l10kai <l10kai2005@gmail.com>
2024-12-05 17:56:22 +0800

    添加多个AtCoder和Luogu题目的实现文件

---
+++ atcoder/abc382/C_Kaiten_Sushi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+
+const char &ln = '\n';
+
+typedef pair<i64, i64> pll;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    
+
+    return 0;
+}
